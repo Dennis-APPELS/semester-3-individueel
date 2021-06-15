285516ce673ef2739ac43c7da0a214ecdafe5175
DennisAPPELS <60885395+Dennis-APPELS@users.noreply.github.com>
2021-06-15 12:59:19 +0200

    changed repo url to raw.url

--- src/main/java/com/databaseapi/demo/DatabaseApiApplication.java
+++ src/main/java/com/databaseapi/demo/DatabaseApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DatabaseApiApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(DatabaseApiApplication.class, args);
     }
